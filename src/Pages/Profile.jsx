f7135823ae403c059f4efe99acdda58ef5087d2a
Abdullah1189 <sajid5396049@gmail.com>
2025-02-03 22:54:52 +0500

    Update Profile.jsx

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -88,7 +88,7 @@ if (loading) {
     return (
       <div className="flex justify-center items-center min-h-screen bg-gray-100">
         <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
-        <Toaster />
+        
       </div>
     );
   }
